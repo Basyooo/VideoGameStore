d9a3b2ba39fd317dab3ee3979faa8e740cea4b1c
DynaBook <DynaBook@DESKTOP-0MC143Q>
2024-06-11 21:45:49 +0300

    hi

--- VideoGameStore/Pages/Payment.cshtml.cs
+++ VideoGameStore/Pages/Payment.cshtml.cs
@@ -1,10 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace VideoGameStore.Pages
 {
     public class paymentModel : PageModel
     {
+        public IActionResult OnPost()
+        {
+            string gameName = "All";
+            string email = Request.Form["email"];
+            string user = Request.Form["user"];
 
+            string connectionString = "Data Source=DESKTOP-0MC143Q\\BASYO; database = VideoGame ; Integrated Security=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            string query = "UPDATE Users SET games_bought =" + "'" + gameName + "'" + "WHERE Email =" + "'" + email + "'" + " OR User_name =" + "'" + user + "'";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.ExecuteNonQuery();
+
+            return RedirectToPage("/succesful");
+        }
     }
 }
