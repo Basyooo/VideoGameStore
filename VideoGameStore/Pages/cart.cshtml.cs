86e0c8bd809bcd76922fe8735040b8369165d821
DynaBook <DynaBook@DESKTOP-0MC143Q>
2025-01-11 08:59:07 +0300

    .

--- VideoGameStore/Pages/cart.cshtml.cs
+++ VideoGameStore/Pages/cart.cshtml.cs
@@ -1,27 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace VideoGameStore.Pages
 {
-    public class cartModel : PageModel
+    public class CartModel : PageModel
     {
-        public IActionResult OnPost()
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+        public decimal TotalPrice { get; set; }
+
+        public void OnGet()
         {
-            string gameName = "All";
-            string email = Request.Form["email"];
-            string user = Request.Form["user"];
+            string connectionString = "Data Source=DESKTOP-0MC143Q\\BASYO; database=VideoGame; Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT Name, Price, ImageUrl FROM Cart";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
 
-            string connectionString = "Data Source=DESKTOP-0MC143Q\\BASYO; database = VideoGame ; Integrated Security=True";
-
-            SqlConnection connection = new SqlConnection(connectionString);
-
-            connection.Open();
-            string query = "UPDATE Users SET games_bought =" + "'" + gameName + "'" + "WHERE Email =" + "'" + email + "'" + " OR User_name =" + "'" + user + "'";
-            SqlCommand command = new SqlCommand(query, connection);
-            command.ExecuteNonQuery();
-
-            return RedirectToPage("/succesful");
+                while (reader.Read())
+                {
+                    CartItems.Add(new CartItem
+                    {
+                        Name = reader.GetString(0),
+                        Price = reader.GetDecimal(1),
+                        ImageUrl = reader.GetString(2)
+                    });
+                    TotalPrice += reader.GetDecimal(1);
+                }
+            }
         }
+
+        public IActionResult OnPostCheckout(string email, string user)
+        {
+            string connectionString = "Data Source=DESKTOP-0MC143Q\\BASYO; database=VideoGame; Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "UPDATE Users SET games_bought = 'All' WHERE Email = @Email OR User_name = @User";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@User", user);
+                command.ExecuteNonQuery();
+            }
+
+            return RedirectToPage("/successful");
+        }
+    }
+
+    public class CartItem
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
